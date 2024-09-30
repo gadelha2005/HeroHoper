2547b3ee5ada4737f7f11daf60ff166d02627dea
Pedro Gadelha <pgpgadelha123@gmail.com>
2024-09-30 12:34:07 -0300

    Commit 3

--- cenas/enemy.gd
+++ cenas/enemy.gd
@@ -5,7 +5,9 @@ class_name Enemy
 
 @export var horizontal_speed = 20
 @export var vertical_speed = 100
+
 @onready var ray_cast_2d = $RayCast2D as RayCast2D
+@onready var animated_sprite_2d = $AnimatedSprite2D as AnimatedSprite2D
 
 func _process(delta):
 	position.x -= horizontal_speed * delta
@@ -13,3 +15,28 @@ func _process(delta):
 	if !ray_cast_2d.is_colliding():
 		position.y += vertical_speed * delta
 
+
+func die():
+	horizontal_speed = 0
+	vertical_speed = 0 
+	animated_sprite_2d.play("dead")
+
+
+func die_from_hit():
+	set_collision_layer_value(3 , false)
+	set_collision_mask_value(1 , false)
+	
+	rotation_degrees = 180
+	horizontal_speed = 0 
+	vertical_speed = 0
+	
+	var die_tween = get_tree().create_tween()
+	die_tween.tween_property(self , "position" , position + Vector2(0 , -25) , .2) 
+	die_tween.tween_property(self , "position" , position + Vector2(0 , 500) , 4) 
+	
+	
+func _on_area_entered(area):
+	if area is Koppa and (area as  Koppa).in_a_shell and (area as Koppa).horizontal_speed != 0 : 
+		die_from_hit()
+		
+	
--- cenas/player.gd
+++ cenas/player.gd
@@ -15,6 +15,14 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var speed = 400
 @export var jump_velocity = -350
 
+@export_group("Stomping enemeis")
+@export var min_stomp_degree = 35
+@export var max_stomp_degree = 145
+@export var stonp_y_velocity = -150
+@export_group("")
+
+
+
 var player_mode = PlayerMode.small
 
 func _process(delta):
@@ -39,3 +47,26 @@ func _process(delta):
 	
 	move_and_slide()
 	
+
+
+func _on_area_2d_area_entered(area):
+	if area is Enemy:
+		handle_enemy_collision(area)
+		
+func handle_enemy_collision(enemy: Enemy):
+	if enemy == null:
+		return 
+		
+	var angle_of_collision = rad_to_deg(position.angle_to_point(enemy.position))
+	
+	if angle_of_collision > min_stomp_degree && max_stomp_degree > angle_of_collision:
+		enemy.die()
+		on_enemy_stomped()
+	else:
+		die()
+		
+func on_enemy_stomped():
+	velocity.y = stonp_y_velocity
+		
+func die():
+	print("Die")
